dee4f65d7d4171e49286671901309ea6dbe65299
JoshMarsden94 <joshmarsden94@outlook.com>
2017-03-20 11:20:41 +0000

    Implement Identity, First & Last, with tests

--- lowbar.js
+++ lowbar.js
@@ -4,7 +4,119 @@ _.identity = function (value) {
   return value;
 };
 
+_.first = function (array, n) {
+    if (!n) return array[0];
+
+    let res = [];
+    let i = 0;
+
+    while (i < n) {
+        res.push(array[i]);
+        i++;
+    }
+
+    return res;
+};
+
+_.last = function (array, n) {
+    if (!n) return array[array.length - 1];
+
+    let res = [];
+    for (let i = array.length - n; i < array.length; i++) {
+        res.push(array[i]);
+    }
+    
+    return res;
+};
+
+_.each = function () {
+
+};
+
+_.indexOf = function () {
+
+};
+
+_.filter = function () {
+
+};
+
+_.reject = function () {
+
+};
+
+_.uniq = function () {
+
+};
+
+_.map = function () {
+
+};
+
+_.pluck = function () {
+
+};
+
+_.reduce = function () {
+
+};
+
+_.contains = function () {
+
+};
+
+_.every = function () {
+
+};
+
+_.some = function () {
+
+};
+
+_.extends = function () {
+
+};
+
+_.defaults = function () {
+
+};
 
 if (typeof module !== 'undefined') {
   module.exports = _;
 }
+
+/* 
+
+1. identity
+2. first
+3. last
+4. each
+5. indexOf
+6. filter
+7. reject
+8. uniq
+9. map
+10. pluck
+11. reduce
+12. contains
+13. every
+14. some
+15. extends
+16. defaults
+
+// Start of Advanced:
+1. indexOf (again, this time with a binary search)
+2. once
+3. memoize
+4. delay
+5. shuffle
+6. invoke
+7. sortBy (NB the Underscore library uses the native JavaScript sort but feel free to use your sort algorithm!)
+8. zip
+9. sortedIndex
+10. flatten
+11. intersection
+12. difference
+13. throttle
+
+*/
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -9,6 +9,7 @@ describe('_', function () {
   it('is an object', function () {
     expect(_).to.be.an('object');
   });
+
   describe('#identity', function () {
     it('is a function', function () {
       expect(_.identity).to.be.a('function');
@@ -17,6 +18,32 @@ describe('_', function () {
       expect(_.identity([1, 2, 4])).to.eql([1, 2, 4]);
     });
   });
+
+  describe('#first', function () {
+    it('is a function', function () {
+      expect(_.first).to.be.a('function');
+    });
+    it('returns the first value of an array when not passed n as a second argument', function () {
+      expect(_.first([1, 2, 3])).to.eql(1);
+    });
+    it('returns the first n values of an array when passed n as a second argument', function () {
+      expect(_.first([1, 2, 3, 4, 5], 3)).to.eql([1, 2, 3]);
+      expect(_.first(['dog', 'cat', 'monkey', 'crocodile'], 2)).to.eql(['dog', 'cat']);
+    });    
+  }); 
+
+  describe('#last', function () {
+    it('is a function', function () {
+      expect(_.last).to.be.a('function');
+    });
+    it('returns the last value of an array when not passed n as a second argument', function () {
+      expect(_.last([1, 2, 3])).to.eql(3);
+    });
+    it('returns the last n values of an array when passed n as a second argument', function () {
+      expect(_.last([1, 2, 3, 4, 5], 3)).to.eql([3, 4, 5]);
+      expect(_.last(['dog', 'cat', 'rabbit', 'monkey', 'crocodile'], 2)).to.eql(['monkey', 'crocodile']);
+    });    
+  });     
 
 
 });
